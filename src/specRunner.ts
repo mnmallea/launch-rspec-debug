daac37249c1849a4a9c67842980aa49f7b3b128c
Martín Mallea <martin.mallea@wolox.com.ar>
2019-10-21 15:16:29 -0300

    Running spec with custom debug configuration

--- src/specRunner.ts
+++ src/specRunner.ts
@@ -4,5 +4,13 @@ import * as vscode from "vscode";
 import { debug } from "util";
 
 export function runSpecFile() {
-    vscode.debug.startDebugging(undefined, 'rspec-debug')
+    const debugConfiguration: vscode.DebugConfiguration = {
+        type: 'Ruby',
+        name: 'Launch',
+        request: 'launch',
+        program: 'bundle',
+        args: ['exec', 'rspec', '-fd', '${file}']
+    }
+    const folder: vscode.WorkspaceFolder | undefined = vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders[0] : undefined
+    vscode.debug.startDebugging(folder, debugConfiguration)
 }
